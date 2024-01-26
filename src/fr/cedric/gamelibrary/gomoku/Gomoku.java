700bc6cb4131feebae2a00cc4d6ed135a87f68d4
Cedric <cedric.atzori@le-campus-numerique.fr>
2024-01-26 14:20:04 +0100

    Ajouter les jeux Gomoku et Puissance4 à la bibliothèque de jeux
    Chaque jeu a été implémenté avec son composant nécessaire - une classe de jeu unique qui adhère maintenant à l'interface 'Game'. Les nouvelles méthodes "play" facilitent la personnalisation future du déroulement du jeu tout en optimisant la lisibilité du code et en conservant les principes d'un code propre.

---
+++ src/fr/cedric/gamelibrary/gomoku/Gomoku.java
@@ -0,0 +1,12 @@
+package fr.cedric.gamelibrary.gomoku;
+
+import fr.cedric.gamelibrary.factory.Game;
+
+public class Gomoku implements Game {
+
+    @Override
+    public void play() {
+        System.out.println("Gomoku");
+
+    }
+}
---
+++ src/fr/cedric/gamelibrary/puissance4/Puissance4.java
@@ -0,0 +1,12 @@
+package fr.cedric.gamelibrary.puissance4;
+
+import fr.cedric.gamelibrary.factory.Game;
+
+public class Puissance4 implements Game {
+
+    @Override
+    public void play() {
+        System.out.println("Puissance4");
+
+    }
+}
